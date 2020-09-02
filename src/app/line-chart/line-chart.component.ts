207bde719f693476fd6e255bdcae348155d42ec2
parmarmanishj2012@gmail.com <parmarmanishj2012@gmail.com>
2020-09-02 21:06:14 +0530

    changes in line chart

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -6,48 +6,121 @@ import * as d3 from 'd3';
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
-  private data = [10, 15, 20, 25, 30];
   private width = 1745;
   private height = 250;
+  private margin = { top: 20, right: 50, bottom: 20, left: 50 };
+  private myData = [{
+    "date": 'January',
+    "value": 400
+  },
+  {
+    "date": 'February',
+    "value": 300
+  },
+  {
+    "date": 'March',
+    "value": 250
+  },
+  {
+    "date": 'April',
+    "value": 450
+  },
+  {
+    "date": 'May',
+    "value": 220
+  },
+  {
+    "date": 'June',
+    "value": 210
+  },
+  {
+    "date": 'July',
+    "value": 250
+  },
+  {
+    "date": 'August',
+    "value": 100
+  },
+  {
+    "date": 'September',
+    "value": 220
+  },
+  {
+    "date": 'October',
+    "value": 360
+  },
+  {
+    "date": 'November',
+    "value": 234
+  },
 
+  {
+    "date": 'December',
+    "value": 290
+  }];
   constructor() { }
 
   ngOnInit(): void {
     this.initSvg();
   }
+
   private initSvg() {
     var svg = d3.select("#lineChart")
       .append("svg")
       .attr("width", this.width)
       .attr("height", this.height);
 
-    var xscale = d3.scaleLinear()
-      .domain([0, d3.max(this.data)])
-      .range([0, this.width - 100]);
-    // var xscale = d3.scaleBand()
-    //               .rangeRound([0, this.width- this.margin.right])
-    //               .padding(0.05);
+    var xscale = d3.scalePoint()
+      .domain(this.myData.map((d: any) => {               // This is what is written on the Axis: from January to December
+        // console.log(d.date)
+        return d.date
+      }))
+      .range([this.margin.left, this.width - this.margin.right]);         // This is where the axis is placed in px
+
     var yscale = d3.scaleLinear()
-      .domain([0, d3.max(this.data)])
-      .range([this.height - 30, 0]);
+      .domain([0, 500])
+      .range([this.height - this.margin.bottom, this.margin.top]);      //reversed
 
-    //  var x_axis = d3.axisBottom()
-    //                     .scale(xscale);
-
-    //  var y_axis = d3.axisLeft()
-    //                     .scale(yscale);
-
+    // For y axis
     svg.append("g")
-      .attr("transform", "translate(50, 10)")
-      // .call(y_axis)
-      .call(d3.axisLeft(yscale));
+      .attr("transform", "translate(" + (this.margin.left) + ",0)")
+      .call(d3.axisLeft(yscale).ticks(6));
 
-    var xAxisTranslate = this.height - 20;
-
+    // For x axis
     svg.append("g")
-      .attr("transform", "translate(50, " + xAxisTranslate + ")")
-      // .call(x_axis)
-      .call(d3.axisBottom(xscale))
+      .attr("transform", "translate(0," + (this.height - this.margin.bottom) + ")")
+      .call(d3.axisBottom(xscale));
 
+    // line generator
+    var valueline = d3.line()
+      .x(function (d, i) {
+        return xscale(d.date);
+      })
+      .y(function (d) {
+        // console.log("y" + d.value)
+        return yscale(d.value);
+      });
+
+    // Append the path, bind the data, and call the line generator
+    svg.append("g").append("path")
+      .data([this.myData])
+      .attr("stroke", "red")
+      .attr("stroke-width", "3")
+      .attr("fill", "none")
+      .attr("d", valueline)
+      .transition()
+      .duration(1000);
+
+    // Appends a circle for each datapoint
+    svg.append("g").selectAll(".dot")
+      .data(this.myData)
+      .enter()
+      .append("circle") // Uses the enter().append() method
+      .attr("fill", "red")
+      .attr("cx", function (d, i) {
+        return xscale(d.date);
+      })
+      .attr("cy", function (d) { return yscale(d.value) })
+      .attr("r", 5);
   }
 }
